2e0dad3edd5756f1cc23890a831aeaebc0ffdbcb
Danny <68480549+DMedina559@users.noreply.github.com>
2025-07-18 08:20:10 -0700

    fix: fix test

--- test/web/test_main.py
+++ test/web/test_main.py
@@ -1,12 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import patch, MagicMock
-import os
-
-# Set the path to the themes directory before importing the app
-themes_path = os.path.join(os.path.dirname(__file__), "themes")
-if not os.path.exists(themes_path):
-    os.makedirs(themes_path)
 
 
 @pytest.fixture(scope="module")
@@ -16,7 +10,6 @@ def client():
         "bedrock_server_manager.instances.get_settings_instance"
     ) as mock_get_settings_instance:
         mock_settings = MagicMock()
-        mock_settings.get.return_value = themes_path
         mock_get_settings_instance.return_value = mock_settings
         from bedrock_server_manager.web.main import app
 
@@ -33,16 +26,6 @@ def test_static_files(client):
     """Test that static files are served."""
     response = client.get("/static/css/base.css")
     assert response.status_code == 200
-
-
-def test_themes_files(client):
-    """Test that themes files are served."""
-    # Create a dummy css file in the themes folder
-    with open(os.path.join(themes_path, "test.css"), "w") as f:
-        f.write("body {background-color: red;}")
-    response = client.get("/themes/test.css")
-    assert response.status_code == 200
-    os.remove(os.path.join(themes_path, "test.css"))
 
 
 def test_openapi_json(client):
